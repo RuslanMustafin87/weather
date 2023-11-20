327b870a08c941a6f5c6caf1995fe3d29dcad0d8
Ruslan Mustafin <ruslanmust87@gmail.com>
2023-11-21 01:35:18 +0500

    handling error

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,20 @@ const API__KEY = '21fb40f9530f2f06808216ebc8193a4a'
 export default {
   data() {
     return {
-      weather: {},
+      weather: null,
       city: 'Chishmy',
+      today: new Date().toLocaleString("ru-RU", {weekday: "short", day: "numeric", month: "long", year: "numeric"})
     }
   },
-  created() {
-    axios(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${API__KEY}&units=metric`)
-      .then(res => {this.weather = res.data; })
-
-    setInterval(() => {
-      axios(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${API__KEY}&units=metric`)
-      .then(res => {this.weather = res.data; })
-    }, 5000)
+  mounted() {
+    this.getWeather();
+    setInterval(this.getWeather, 600000)
   },
   methods: {
     getWeather(){
       axios(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${API__KEY}&units=metric`)
         .then(res => this.weather = res.data)
+        .catch(err => this.weather = null)
     }
   },
   computed: {
@@ -33,15 +30,28 @@ export default {
 </script>
 
 <template>
-<input type="text" v-model="city" @keyup.enter="getWeather">
+<input class="input-city" type="text" v-model="city" @keyup.enter="getWeather">
+
 <br>
-<button type="button" style="margin: 20px;" @click="getWeather">Get weather</button>
-<div class="temp">Temperature: {{ weather.main.temp }} &deg</div>
-<div class="wind">Wind: {{ weather.wind.speed }} m/s</div>
-<div class="weather">{{ weather.weather[0].main }}</div>
-<img :src="src" alt="">
+<button type="button" style="margin: 20px;" @click="getWeather">Узнать погоду</button>
+<div v-if="weather?.weather">
+  <div class="temp">{{ Math.round(weather.main.temp) }} &deg C</div>
+  <div class="city"> {{ weather.name }}</div>
+  <div class="date"> {{ today }}</div>
+  <div class="wind">Ветер:  {{ weather.wind.speed }} м/с</div>
+  <!-- <div class="weather">{{ weather.weather[0].main }}</div> -->
+  <img :src="src" alt="">
+</div>
+<div v-else>City not find</div>
 </template>
 
 <style scoped>
-
+.input-city{
+  width: 250px;
+  height: 30px;
+  padding-left: 10px;
+}
+.temp {
+  font-size: 2rem;
+}
 </style>
