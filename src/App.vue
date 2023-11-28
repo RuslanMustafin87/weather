01a36df6035da7e2c02d701630a926aa2eb8150e
Ruslan Mustafin <ruslanmust87@gmail.com>
2023-11-29 00:06:50 +0500

    composition api

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,22 @@
-<script>
+<script setup>
+import { ref } from 'vue';
 import axios from 'axios'
 const API__KEY = '21fb40f9530f2f06808216ebc8193a4a'
 
-export default {
-  data() {
-    return {
-      weather: null,
-      city: '',
-      today: new Date().toLocaleString("ru-RU", {weekday: "short", day: "numeric", month: "long", year: "numeric"})
-    }
-  },
-  mounted() {
-    this.getWeather();
-    setInterval(this.getWeather, 600000)
-  },
-  methods: {
-    getWeather(){
-      axios(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${API__KEY}&units=metric`)
-        .then(res => this.weather = res.data)
-        .catch(err => this.weather = null)
-    }
-  },
-  computed: {
-    src(){
-      // return`https://openweathermap.org/img/wn/${this.weather.weather[0].icon}@2x.png`
-      return `src/assets/${this.weather.weather[0].icon}.svg`
-    }
-  }
+let weather = ref(null);
+let city = ref('');
+let today = ref(new Date().toLocaleString("ru-RU", {weekday: "short", day: "numeric", month: "long", year: "numeric"}));
+
+const getWeather = () => {
+  axios(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${API__KEY}&units=metric`)
+    .then(res => weather.value = res.data)
+    .catch(err => weather.value = null)
 }
+
 </script>
 
 <template>
 <input class="input-city" type="text" placeholder="Введите город" v-model="city" @keyup.enter="getWeather">
-
 <br>
 <button type="button" style="margin: 20px;" @click="getWeather">Узнать погоду</button>
 <div v-if="weather?.weather">
@@ -41,7 +25,7 @@ export default {
   <div class="date"> {{ today }}</div>
   <div class="wind">Ветер:  {{ weather.wind.speed }} м/с</div>
   <div class="weather">{{ weather.weather[0].main }}</div>
-  <img :src="`./assets/img/${this.weather.weather[0].icon}.svg`" alt="">
+  <img :src="`./assets/img/${weather.weather[0].icon}.svg`" alt="">
 </div>
 <div v-else>Город не найден</div>
 </template>
